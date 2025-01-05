02683df6a93299b4bffce24a3546d32b407c249d
Fynn Raddatz <fynn.raddatz@gmail.com>
2025-01-05 22:35:25 +0100

    handle STM32 types

--- SX1278.hpp
+++ SX1278.hpp
@@ -1,5 +1,29 @@
 #pragma once
 
+#include "SX1278_reg.hpp"
+
+#include <cstdint>
+#include "I2C.h"
+
+// Use the following flags for compiling the right library, e.g.: -D STM32F1
+#if defined(STM32F0)
+#include "stm32f0xx_hal.h"
+#elif defined(STM32F1)
+#include "stm32f1xx_hal.h"
+#elif defined(STM32F2)
+#include "stm32f2xx_hal.h"
+#elif defined(STM32F3)
+#include "stm32f3xx_hal.h"
+#elif defined(STM32F4)
+#include "stm32f4xx_hal.h"
+#elif defined(STM32F7)
+#include "stm32f7xx_hal.h"
+#elif defined(STM32H7)
+#include "stm32h7xx_hal.h"
+#else
+#error "Unsupported STM32 microcontroller. Make sure you build with -D STM32F1 for example!"
+#endif
+
 class SX1278
 {
 public:
